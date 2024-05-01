022acace9b5e4a01b21f29cc804ae6001cdf8368
Justin Kuan <sc21jzhk@leeds.ac.uk>
2024-05-01 15:05:43 +0100

    FCN Resnet model now calls a FCN_ResNet50 object

--- architectures/fcn_resnet50.py
+++ architectures/fcn_resnet50.py
@@ -1,16 +1,23 @@
 # https://github.com/sovit-123/CamVid-Image-Segmentation-using-FCN-ResNet50-with-PyTorch/blob/master/model.py
 import torchvision.models as models
+from torchvision.models.segmentation.fcn import FCN
 import torch.nn as nn
 
-def Model(weights=None, requires_grad=True):
-    model = models.segmentation.fcn_resnet50(weights=weights, progress=True)
+def Model():
+    obj = FCN_ResNet50()
+    return obj.model
 
-    for param in model.parameters():
-        param.requires_grad = requires_grad
+class FCN_ResNet50:
+    def __init__(self, weights=None, requires_grad=True):
+        self.weights = weights
+        self.model = models.segmentation.fcn_resnet50(weights=self.weights, progress=True)
+        
+        self.model.requires_grad = requires_grad
+        for param in self.model.parameters():
+            param.requires_grad = self.model.requires_grad
 
-    model.classifier[4] = nn.Conv2d(512, 32, kernel_size=(1,1))
-    
-    if model.aux_classifier is not None:
-        model.aux_classifier[4] = nn.Conv2d(256, 32, kernel_size=(1,1))
-
-    return model
+        self.model.classifier[4] = nn.Conv2d(512, 32, kernel_size=(1,1))
+        if self.model.aux_classifier is not None:
+            self.model.aux_classifier[4] = nn.Conv2d(256, 32, kernel_size=(1,1))
+        
+        self.model.name = 'fcn_resnet50'
